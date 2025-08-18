c8ed159d76b3deb8818e4ed03972d84b85f78711
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-08-18 18:00:09 +0200

    added equals() and hashCode(), made class Serializable

--- src/main/java/pl/kurs/watercontainers/models/Employee.java
+++ src/main/java/pl/kurs/watercontainers/models/Employee.java
@@ -1,6 +1,9 @@
 package pl.kurs.watercontainers.models;
 
-public class Employee {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Employee implements Serializable {
 
 
     private String firstName;
@@ -16,8 +19,42 @@ public class Employee {
         this.salary = salary;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, salary);
+    }
 
     @Override
     public String toString() {
